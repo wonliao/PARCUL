64b1197531ef345d5862727c3885d063067f0c5e
wonliao <wonliao1117@gmail.com>
2016-04-17 13:26:28 +0900

    Project's Initial Commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,7 @@
-
-var isRelease = true;
-
-var api_url = "http://livebricks.incdoor.com/";
-document.addEventListener("init", function(event) {
-    if(isRelease == false) {
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-            //api_url = "http://192.168.0.157/";    // mobile
-        } else {
-            api_url = "https://index-server/";   // PC
-        }
-    }
-});
-
 ons.bootstrap();
+
+ons.disableAutoStatusBarFill();
+
+if(ons.platform.isIOS()) {
+	$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'css/ios.css') );
+}
